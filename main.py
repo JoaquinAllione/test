8bf67a9937cdbd86a27a0d6b7a90265deeb015c0
JoaquinAllione <joaquin.allione@hotmail.com>
2023-11-27 14:43:51 -0300

    segundo comiit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello")
+import logging
+
+logger = logging.getLogger("Main APP")
+logger.setLevel(logging.DEBUG)
+
+def Imprimir(texto:str):
+    if isinstance(texto,str):
+        print(texto)
+        logger.info("Se imprimió correctamente.")
+    else:
+        logger.error("Debe ser un string.")
+
+Imprimir("Hola")
